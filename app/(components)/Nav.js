a2000a48ea3468be515ca79d607d80641f5f84a3
ulxsth <ulxsth@gmail.com>
2024-01-29 22:07:42 +0900

    update: navに項目を追加

--- app/(components)/Nav.js
+++ app/(components)/Nav.js
@@ -1,8 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <div>
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/TicketPage/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+
+      <div>
+        <p className="text-default-text">yotu@example.com</p>
+      </div>
+    </nav>
   )
 }
 
