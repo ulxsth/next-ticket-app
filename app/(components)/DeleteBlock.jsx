70e3ce5c5eb4b440d621aeaed3ace60932d1d710
ulxsth <ulxsth@gmail.com>
2024-01-29 22:26:58 +0900

    fix: add import

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faX } from '@fortawesome/free-solid-svg-icons'
 
 const DeleteBlock = () => {
   return (
