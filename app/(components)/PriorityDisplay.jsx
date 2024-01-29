e79b26005add7892531b1ecddf7958ff322e79a0
ulxsth <ulxsth@gmail.com>
2024-01-29 22:27:42 +0900

    add: priority display

---
+++ app/(components)/PriorityDisplay.jsx
@@ -0,0 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+import { faFire } from '@fortawesome/free-solid-svg-icons'
+
+const PriorityDisplay = () => {
+  return (
+    <div className='flex justify-start align-baseline'>
+      <FontAwesomeIcon icon={faFire} className='text-red-400' />
+      <FontAwesomeIcon icon={faFire} className='text-red-400' />
+      <FontAwesomeIcon icon={faFire} className='text-red-400' />
+      <FontAwesomeIcon icon={faFire} className='text-red-400' />
+      <FontAwesomeIcon icon={faFire} className='text-red-400' />
+    </div>
+  )
+}
+
+export default PriorityDisplay
