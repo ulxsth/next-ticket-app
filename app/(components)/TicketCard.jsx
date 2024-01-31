14705f72ba4e02f84d47bb0c984dd271ee031d06
ulxsth <ulxsth@gmail.com>
2024-01-31 16:43:18 +0900

    update: チケットのレイアウトを整理した

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -14,10 +14,18 @@ const TicketCard = () => {
         </div>
       </div>
       <h4>Ticket Title</h4>
-      <hr className='h-px border-0 bg-page mb-2' />
-      <p className='whitespace-pre-wrap'>this is the ticket description!</p>
-      <ProgressDisplay />
-      <StatusDisplay />
+      <hr className="h-px border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">this is the ticket description!</p>
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className='text-xs my-1'>08/31/23 10:43</p>
+          <ProgressDisplay />
+        </div>
+        <div className='ml-auto flex items-end'>
+          <StatusDisplay />
+        </div>
+      </div>
     </div>
   );
 }
