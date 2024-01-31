5f3ac9854812c5eab73ca5c18a15f2b674ceb894
ulxsth <ulxsth@gmail.com>
2024-01-31 16:36:41 +0900

    update: チケットカードにステータスを追加し、見た目を整えた

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import DeleteBlock from './DeleteBlock'
 import PriorityDisplay from './PriorityDisplay'
 import ProgressDisplay from './ProgressDisplay';
+import StatusDisplay from './StatusDisplay'
 
 const TicketCard = () => {
   return (
-    <div>
-      <DeleteBlock />
-      <PriorityDisplay />
+    <div className="flex flex-col bg-card hober:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay />
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
+      <h4>Ticket Title</h4>
+      <hr className='h-px border-0 bg-page mb-2' />
+      <p className='whitespace-pre-wrap'>this is the ticket description!</p>
       <ProgressDisplay />
+      <StatusDisplay />
     </div>
   );
 }
