1caebb98005ab3e2754f93b7cc06a3f6fe53982f
ulxsth <ulxsth@gmail.com>
2024-02-01 20:44:54 +0900

    update: 優先度の値を取得し、それに応じて表示を変更するようにした

--- app/(components)/PriorityDisplay.jsx
+++ app/(components)/PriorityDisplay.jsx
@@ -1,18 +1,33 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-import { faFire } from '@fortawesome/free-solid-svg-icons'
+import { faFire } from "@fortawesome/free-solid-svg-icons";
 
-const PriorityDisplay = () => {
+const PriorityDisplay = ({ priority }) => {
   return (
-    <div className='flex justify-start align-baseline'>
-      <FontAwesomeIcon icon={faFire} className='text-red-400' />
-      <FontAwesomeIcon icon={faFire} className='text-red-400' />
-      <FontAwesomeIcon icon={faFire} className='text-red-400' />
-      <FontAwesomeIcon icon={faFire} className='text-red-400' />
-      <FontAwesomeIcon icon={faFire} className='text-red-400' />
+    <div className="flex justify-start align-baseline">
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 0 ? "text-red-400" : "text-slate-400"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 1 ? "text-red-400" : "text-slate-400"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 2 ? "text-red-400" : "text-slate-400"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 3 ? "text-red-400" : "text-slate-400"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 4 ? "text-red-400" : "text-slate-400"}`}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default PriorityDisplay
+export default PriorityDisplay;
