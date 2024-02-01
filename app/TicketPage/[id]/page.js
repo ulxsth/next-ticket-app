ed97dc30c69d789ede3678d8ae1dda0b43505299
ulxsth <ulxsth@gmail.com>
2024-02-01 23:31:01 +0900

    update: チケットの編集ページを実装

--- app/TicketPage/[id]/page.js
+++ app/TicketPage/[id]/page.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import TicketForm from '../../(components)/TicketForm'
 
-const TicketPage = ({ params }) => {
+const getTicketById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      cache: "no-store"
+    })
+  } catch (error) {
+    throw new Error("Failed to get ticket.")
+  }
+
+  return res.json();
+}
+
+const TicketPage = async ({ params }) => {
+  const EDITMODE = params.id === "new" ? false : true;
+  let updateTicketData = {};
+
+  if (EDITMODE) {
+    updateTicketData = await getTicketById(param.id);
+    console.log(updateTicketData)
+  }
   return (
     <TicketForm />
   )
