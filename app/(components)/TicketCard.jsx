1f369db685267e3398075ea054f5fa57f263deb7
ulxsth <ulxsth@gmail.com>
2024-02-01 23:21:06 +0900

    fix: ホバー時にチケットの色が変わらない不具合を修正

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -3,6 +3,7 @@ import DeleteBlock from "./DeleteBlock";
 import PriorityDisplay from "./PriorityDisplay";
 import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
+import Link from "next/link";
 
 const TicketCard = ({ ticket }) => {
   const formatTimestamp = (timestamp) => {
@@ -22,26 +23,29 @@ const TicketCard = ({ ticket }) => {
   };
 
   return (
-    <div className="flex flex-col bg-card hober:bg-card-hover rounded-md shadow-lg p-3 m-2">
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
         <PriorityDisplay priority={ticket.priority} />
         <div className="ml-auto">
           <DeleteBlock id={ticket._id} />
         </div>
       </div>
-      <h4>{ticket.title}</h4>
-      <hr className="h-px border-0 bg-page mb-2" />
-      <p className="whitespace-pre-wrap">{ticket.description}</p>
-      <div className="flex-grow"></div>
-      <div className="flex mt-2">
-        <div className="flex flex-col">
-          <p className="text-xs my-1">{formatTimestamp(ticket.createdAt)}</p>
-          <ProgressDisplay progress={ticket.progress} />
+
+      <Link href={`/TicketPage/${ticket._id}`} style={{display: "contents"}}>
+        <h4>{ticket.title}</h4>
+        <hr className="h-px border-0 bg-page mb-2" />
+        <p className="whitespace-pre-wrap">{ticket.description}</p>
+        <div className="flex-grow"></div>
+        <div className="flex mt-2">
+          <div className="flex flex-col">
+            <p className="text-xs my-1">{formatTimestamp(ticket.createdAt)}</p>
+            <ProgressDisplay progress={ticket.progress} />
+          </div>
+          <div className="ml-auto flex items-end">
+            <StatusDisplay status={ticket.status} />
+          </div>
         </div>
-        <div className="ml-auto flex items-end">
-          <StatusDisplay status={ticket.status} />
-        </div>
-      </div>
+      </Link>
     </div>
   );
 };
